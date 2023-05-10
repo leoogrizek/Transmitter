eeeed7977746a802f2a9e38844dcd3ff6ed05b83
leoogrizek <35898289+leoogrizek@users.noreply.github.com>
2023-05-10 18:31:55 +0200

    transmission test

--- transmitter_test_single/main.cpp
+++ transmitter_test_single/main.cpp
@@ -5,6 +5,7 @@
 
 #include "rf.h"
 #include "uart.h"
+#include "spi.h"
 
 uint8_t value = 0;
 
@@ -13,14 +14,20 @@ int main(void) {
 	uart_init(9600);
 	spi_init();
 	
-	while (1) {
-		nrf24_write_register(CONFIG, 0b00001000);
-		value = nrf24_read_register(CONFIG);
-		uart_print_binary(value);
-		uart_newline();
-		_delay_ms(1000);
-		
+	uint8_t address[5]={0x01, 0x02, 0x03, 0x04, 0x05};
 
+
+	nrf24_set_rx_tx_address(address, 0, 5);
+	nrf24_write_register(RX_ADDR_P0, 0x01);
+	
+	uint8_t value=42;
+	while (1) {
+		spi_transfer(W_TX_PAYLOAD);
+		spi_transfer(value);
+		PORTB |= (1 << CE);
+		_delay_us(15);
+		PORTB &= ~(1 << CE);
+		_delay_ms(1000);
 	}
 	
 	return 0;
